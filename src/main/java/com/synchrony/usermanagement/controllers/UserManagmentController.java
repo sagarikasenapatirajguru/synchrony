b2aebe346cad865690d47ebad588f352540c84eb
sagarika <pravashrajguru@gmail.com>
2023-11-11 01:28:57 +0530

    Implemented Authentication using spring security

--- src/main/java/com/synchrony/usermanagement/controllers/UserManagmentController.java
+++ src/main/java/com/synchrony/usermanagement/controllers/UserManagmentController.java
@@ -5,14 +5,12 @@ import com.synchrony.usermanagement.models.User;
 import com.synchrony.usermanagement.models.UserDto;
 import com.synchrony.usermanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,18 +20,24 @@ public class UserManagmentController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
     @GetMapping("/register")
     public String register(Model model) {
-        model.addAttribute("user",new UserDto());
+        model.addAttribute("user", new UserDto());
         return "register";
     }
+
     @PostMapping("/register/save")
-    public String registration( @ModelAttribute("user") UserDto user,
+    public String registration(@ModelAttribute("user") UserDto user,
                                BindingResult result,
-                               Model model){
+                               Model model) {
         User existing = userService.findUserByLogin(user.getLogin());
         if (existing != null) {
-            result.rejectValue("email", null, "There is already an account registered with that email");
+            result.rejectValue("login", null, "There is already an account registered with that email");
         }
         if (result.hasErrors()) {
             model.addAttribute("user", user);
@@ -42,8 +46,9 @@ public class UserManagmentController {
         userService.saveUser(user);
         return "redirect:/register?success";
     }
+
     @GetMapping("/users")
-    public String listRegisteredUsers(Model model){
+    public String listRegisteredUsers(Model model) {
         List<User> users = userService.findAllUser();
         model.addAttribute("users", users);
         return "users";
